201164aceac45279b47c3f869a5ea7d876d87d3a
Lior <45363723+HirboSH@users.noreply.github.com>
2022-05-28 13:11:12 +0300

    ⚒️ Better Typing and Interfaces
    ⚒️ Better Typing and Interfaces

--- interfaces/ExtendedClient.ts
+++ interfaces/ExtendedClient.ts
@@ -1,9 +1,21 @@
-import type { Client } from "discord.js";
+import type { Client, Collection, CommandInteraction, GuildMember, ApplicationCommandDataResolvable } from "discord.js";
 
 interface ExtendedClient extends Client {
-    config?: any,
-    commands?: any,
-    commandsArr?: any
+    config: NodeJS.ProcessEnv,
+    commands: Collection<string, Command>,
+    commandsArr: Command[]
 }
 
-export default ExtendedClient;
+type Command = ApplicationCommandDataResolvable & {
+    run(client: ExtendedClient, interaction: CommandInteractionWithGuildMember, args: unknown[]): void
+}
+
+type CommandInteractionWithGuildMember = CommandInteraction & {
+    guildMember?: GuildMember
+}
+
+export {
+    ExtendedClient,
+    Command,
+    CommandInteractionWithGuildMember
+};
