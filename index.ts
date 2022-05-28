9f0ec835cfefbc11f6b7a46039343e6bfa371c58
Lior <45363723+HirboSH@users.noreply.github.com>
2022-05-28 13:15:22 +0300

    ⚒️ Updated the Interface import, and ExtendedClient's creation
    ⚒️ Updated the Interface import, and ExtendedClient's creation

--- index.ts
+++ index.ts
@@ -3,16 +3,16 @@ dotenv.config();
 
 import { Client, Intents, Collection } from "discord.js";
 
-import type ExtendedClient from "./interfaces/ExtendedClient.js";
+import type { ExtendedClient, Command } from "./interfaces/interfaces.js";
 
 import commands from "./structures/command.js";
 import events from "./structures/event.js";
 
-const client: ExtendedClient = new Client({ intents: [new Intents(32767)] });
-
-client.config = process.env;
-client.commands = new Collection();
-client.commandsArr = [];
+const client: ExtendedClient = Object.assign(new Client({ intents: [new Intents(32767)] }), {
+    config: process.env,
+    commands: new Collection<string, Command>(),
+    commandsArr: [],
+});
 
 commands(client);
 events(client);
